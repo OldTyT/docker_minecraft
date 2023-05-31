7d68cd978a321ff592762f15960bdc1df5f2967f
OldTyT <nezlobin118@gmail.com>
2023-05-31 21:19:52 +0800

    chore: added msg for print status restored world

--- world_clone.py
+++ world_clone.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 
 def main():
+    print("Starting restored worlds.")
     worlds = os.getenv("WORLDS")
     if str(worlds) == "None":
         print("World set is None. Skiping clone worlds.")
@@ -9,5 +10,6 @@ def main():
     subprocess.run(f"/usr/bin/rclone copy {os.getenv('S3_BUCKET')}:{os.getenv('S3_BUCKET')}/worlds/{worlds}.tar.bz2 /tmp/".split())
     subprocess.run(f"tar -xf /tmp/{worlds}.tar.bz2 -C /app".split())
     subprocess.run(f"rm /tmp/{worlds}.tar.bz2".split())
+    print("Worlds successfully restored.")
 
 main()
