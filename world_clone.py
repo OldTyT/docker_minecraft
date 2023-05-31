1a648bf3af27dcb90ae0bdf6284e03f72d186da8
OldTyT <nezlobin118@gmail.com>
2023-05-31 21:15:31 +0800

    chore: fix rclone copy path

--- world_clone.py
+++ world_clone.py
@@ -6,7 +6,7 @@ def main():
     if str(worlds) == "None":
         print("World set is None. Skiping clone worlds.")
         exit()
-    subprocess.run(f"/usr/bin/rclone copy {os.getenv('S3_BUCKET')}:{os.getenv('S3_BUCKET')}/worlds/{worlds}.tar.bz2 /tmp/{worlds}.tar.bz2".split())
+    subprocess.run(f"/usr/bin/rclone copy {os.getenv('S3_BUCKET')}:{os.getenv('S3_BUCKET')}/worlds/{worlds}.tar.bz2 /tmp/".split())
     subprocess.run(f"tar -xf /tmp/{worlds}.tar.bz2 -C /app".split())
     subprocess.run(f"rm /tmp/{worlds}.tar.bz2".split())
 
